556a15ffa3513a73612759434b371f7447d10748
dhruv <gambhirdhruv03@gmail.com>
2024-06-15 00:53:36 +0800

    added image back

--- app/Collage.js
+++ app/Collage.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 
 function Collage() {
-    const [image, setImage] = useState("/images.jpg");
+    const [image, setImage] = useState("/images/1.jpg");
     return <Image src={image} alt="image" height="400" width="400" />;
 }
 export default Collage;
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,19 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { initializeApp } from "firebase/app";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUb3cVSTAPC4pwi-Jbef1cuA8Ppz2al4U",
+    authDomain: "thismatters-24fb3.firebaseapp.com",
+    projectId: "thismatters-24fb3",
+    storageBucket: "thismatters-24fb3.appspot.com",
+    messagingSenderId: "1024864182441",
+    appId: "1:1024864182441:web:091301dc6b7c654be5c9b8",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const metadata = {
     title: "thismatters.com",
