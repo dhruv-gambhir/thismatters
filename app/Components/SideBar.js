038965e4a0a849563e172d12f110ecafe3c0b5c7
dhruv <gambhirdhruv03@gmail.com>
2024-06-20 15:39:34 +0800

    Sidebar uses router.push() now

--- app/Components/SideBar.js
+++ app/Components/SideBar.js
@@ -1,20 +1,33 @@
+"use client";
+
 import Title from "@/app/Components/Title";
+import { useRouter } from "next/navigation";
 
 export default function SideBar() {
+    const router = useRouter();
+
     return (
         <div className="flex flex-col items-cente w-1/6 m-2 pt-4 border-r border-r-2 border-r-black">
             <Title />
-            <button className="border-solid border-2 border-black rounded m-2 w-10/12">
-                <a href="/home">Home</a>
+            <button
+                className="border-solid border-2 border-black rounded m-2 w-10/12"
+                onClick={() => router.push("/")}>
+                Home
             </button>
-            <button className="border-solid border-2 border-black rounded m-2 w-10/12">
-                <a href="/messages">Messages</a>
+            <button
+                className="border-solid border-2 border-black rounded m-2 w-10/12"
+                onClick={() => router.push("/messages")}>
+                Messages
             </button>
-            <button className="border-solid border-2 border-black rounded m-2 w-10/12">
-                <a href="/profile">Profile</a>
+            <button
+                className="border-solid border-2 border-black rounded m-2 w-10/12"
+                onClick={() => router.push("/profile")}>
+                Profile
             </button>
-            <button className="border-solid border-2 border-black rounded m-2 w-10/12">
-                <a href="/settings">Settings</a>
+            <button
+                className="border-solid border-2 border-black rounded m-2 w-10/12"
+                onClick={() => router.push("/settings")}>
+                Settings
             </button>
         </div>
     );
