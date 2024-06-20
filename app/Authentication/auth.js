d8fd7e54c6b12704f933d296d50238cff37b4fb4
dhruv <gambhirdhruv03@gmail.com>
2024-06-20 16:42:25 +0800

    api folder added

--- app/Authentication/auth.js
+++ app/Authentication/auth.js
@@ -1,5 +1,4 @@
 import { auth } from "./firebaseConfig";
-import { db } from "@vercel/postgres";
 
 import {
     signInWithEmailAndPassword,
@@ -36,24 +35,6 @@ export const signup = async (email, password) => {
         throw error;
     }
 };
-
-export default async function addUserToDB(request, respone) {
-    const client = db.connect;
-
-    try {
-        await client.query(
-            `CREATE TABLE Users (Email varchar(255), Password varchar(255));`
-        );
-        await client.query(
-            `INSERT INTO Users (Email,Password) VALUES ("dhruv@vercel.com", "vercel123")`
-        );
-    } catch {
-        console.log("not added");
-    }
-
-    const users = await client.sql`SELECT * FROM Users`;
-    console.log(users);
-}
 
 export const logout = async () => {
     try {
---
+++ app/api/test/routs.js
@@ -0,0 +1,3 @@
+export function GET(req, res) {
+    Response.json({ Msg: "your message" }, { status: 200 });
+}
