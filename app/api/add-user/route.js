19cd8c580c6085516295dac0283eacf3db4b08d2
dhruv <gambhirdhruv03@gmail.com>
2024-06-21 13:58:48 +0800

    error handling in api routes

--- app/api/add-user/route.js
+++ app/api/add-user/route.js
@@ -4,7 +4,11 @@ import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
 export async function GET() {
-    const addUser =
-        await sql`INSERT INTO users (email,password) VALUES ('dhruv@vercel.com', 'vercel123')`;
-    return NextResponse.json(addUser);
+    try {
+        const addUser =
+            await sql`INSERT INTO users (email,password) VALUES ('dhruv@vercel.com', 'vercel123')`;
+        return NextResponse.json(addUser);
+    } catch {
+        return NextResponse.json("User exists elready");
+    }
 }
--- app/api/create-users-table/route.js
+++ app/api/create-users-table/route.js
@@ -4,7 +4,11 @@ import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 
 export async function GET() {
-    const createTable =
-        await sql`CREATE TABLE users (user_id SERIAL PRIMARY KEY, email VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL);`;
-    return NextResponse.json(createTable);
+    try {
+        const createTable =
+            await sql`CREATE TABLE users (user_id SERIAL PRIMARY KEY, email VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL);`;
+        return NextResponse.json(createTable);
+    } catch {
+        return NextResponse.json("Table exists already");
+    }
 }
