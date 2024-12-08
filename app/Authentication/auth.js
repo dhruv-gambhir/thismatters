bdf2a47dbe586823b57b6a9cbd756c1dcb8c7661
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2024-12-08 23:56:56 +0530

    Change password function added

--- app/Authentication/auth.js
+++ app/Authentication/auth.js
@@ -4,6 +4,7 @@ import {
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
     signOut,
+    updatePassword,
 } from "firebase/auth";
 
 export const login = async (email, password) => {
@@ -40,5 +41,29 @@ export const logout = async () => {
     } catch (error) {
         console.error("Error logging out:", error);
         throw error;
+    }
+};
+
+export const changePassword = async (currentPassword, newPassword) => {
+    const user = auth.currentUser;
+
+    if (user) {
+        // Re-authenticate the user
+        const credential = EmailAuthProvider.credential(
+            user.email,
+            currentPassword
+        );
+
+        try {
+            await reauthenticateWithCredential(user, credential);
+            // Update the password
+            await updatePassword(user, newPassword);
+            console.log("Password updated successfully.");
+        } catch (error) {
+            console.error("Error updating password:", error);
+            throw error;
+        }
+    } else {
+        throw new Error("No user is currently signed in.");
     }
 };
